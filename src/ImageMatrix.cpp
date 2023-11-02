58d1afba33256a0ed2f4cc92901ccc7683f0d60b
Taylan Özdoğan <ozdogantaylan@gmail.com>
2023-11-02 23:57:07 +0300

    ImageMatrix class done for now.

--- src/ImageMatrix.cpp
+++ src/ImageMatrix.cpp
@@ -4,12 +4,28 @@
 
 // Default constructor
 ImageMatrix::ImageMatrix(){
+    // Initialize all the values to 0
+    height = 0;
+    width = 0;
+    data = nullptr;
     
 }
 
 
 // Parameterized constructor for creating a blank image of given size
 ImageMatrix::ImageMatrix(int imgHeight, int imgWidth) : height(imgHeight), width(imgWidth) {
+    // Allocate memory for the matrix
+    data = new double*[imgHeight];
+    for (int i = 0; i < imgHeight; ++i) {
+        data[i] = new double[imgWidth];
+    }
+
+    // Initialize all the values to 0
+    for (int i = 0; i < imgHeight; ++i) {
+        for (int j = 0; j < imgWidth; j++) {
+            data[i][j] = 0;
+        }
+    }
 
 }
 
@@ -41,6 +57,11 @@ ImageMatrix::ImageMatrix(const std::string &filepath) {
 
 // Destructor
 ImageMatrix::~ImageMatrix() {
+    // Deallocate memory
+    for (int i = 0; i < height; ++i) {
+        delete[] data[i];
+    }
+    delete[] data;
 }
 
 // Parameterized constructor - direct initialization with 2D matrix
@@ -64,11 +85,50 @@ ImageMatrix::ImageMatrix(const double** inputMatrix, int imgHeight, int imgWidth
 
 // Copy constructor
 ImageMatrix::ImageMatrix(const ImageMatrix &other) {
-    
+    // Get the dimensions of the input image
+    height = other.height;
+    width = other.width;
+
+    // Allocate memory for the matrix
+    data = new double*[height];
+    for (int i = 0; i < height; ++i) {
+        data[i] = new double[width];
+    }
+
+    // Copy data from other to data
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; j++) {
+            data[i][j] = other.data[i][j];
+        }
+    }
 }
 
 // Copy assignment operator
 ImageMatrix& ImageMatrix::operator=(const ImageMatrix &other) {
+    if (this == &other) {
+        return *this; // self-assignment check
+    }
+
+    // Deallocate old memory
+    if (data != nullptr) {
+        for (int i = 0; i < height; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    // Copy from other
+    height = other.height;
+    width = other.width;
+    data = new double*[height];
+    for (int i = 0; i < height; ++i) {
+        data[i] = new double[width];
+        for (int j = 0; j < width; j++) {
+            data[i][j] = other.data[i][j];
+        }
+    }
+
+    return *this;
     
 }
 
@@ -78,23 +138,68 @@ ImageMatrix& ImageMatrix::operator=(const ImageMatrix &other) {
 
 // Overloaded operator + to add two matrices
 ImageMatrix ImageMatrix::operator+(const ImageMatrix &other) const {
+    // Check if the dimensions of the two matrices are same
+    if (height != other.height || width != other.width) {
+        std::cout << "Error: Dimensions of the two matrices are not same." << std::endl;
+        exit(1);
+    }
+
+    // Create a new matrix to store the result
+    ImageMatrix result(height, width);
+
+    // Add the two matrices
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; j++) {
+            result.data[i][j] = data[i][j] + other.data[i][j];
+        }
+    }
+
+    return result;
     
 }
 
 // Overloaded operator - to subtract two matrices
 ImageMatrix ImageMatrix::operator-(const ImageMatrix &other) const {
+    // Check if the dimensions of the two matrices are same
+    if (height != other.height || width != other.width) {
+        std::cout << "Error: Dimensions of the two matrices are not same." << std::endl;
+        exit(1);
+    }
+
+    // Create a new matrix to store the result
+    ImageMatrix result(height, width);
+
+    // Subtract the two matrices
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; j++) {
+            result.data[i][j] = data[i][j] - other.data[i][j];
+        }
+    }
+
+    return result;
 
 }
 
 // Overloaded operator * to multiply a matrix with a scalar
 ImageMatrix ImageMatrix::operator*(const double &scalar) const {
+    // Create a new matrix to store the result
+    ImageMatrix result(height, width);
+
+    // Multiply the matrix with the scalar
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; j++) {
+            result.data[i][j] = data[i][j] * scalar;
+        }
+    }
+
+    return result;
 
 }
 
 
 // Getter function to access the data in the matrix
 double** ImageMatrix::get_data() const {
-
+    return data;
 }
 
 // Getter function to access the data at the index (i, j)
